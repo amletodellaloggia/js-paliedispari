a6070ca347b396b361a925652a4d6c39121e977c
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-07 14:24:03 +0200

    [add] function, prompt, variables, methods

--- 01 - Palindroma/js/script.js
+++ 01 - Palindroma/js/script.js
@@ -2,3 +2,15 @@
 // PALINDROMA
 
 
+// Creo funzione che verifica se la parola inserita dall'utente è palindroma
+
+function verifyPalindrome() {
+const userWord = prompt(`Inserisci una parola:`)
+
+// Per evitare che la parola non passi la verifica per via delle maiuscole, la converto in lowercase e la salvo nella sua variabile
+const lowerCaseWord =  userWord.toLowerCase();
+
+// Per verificare che scorrendo quindi i caratteri salvati in lowerCaseWord al contrario siano tutti nella stessa posizione, prima trasformo la stringa in un array, la inverto, e poi la trasformo di nuovo in una stringa
+const reversedWord = lowerCaseWord.split("").reverse().join("");
+}
+
