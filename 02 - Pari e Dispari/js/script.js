39f4440a54a5e280f31df24a8e2bb8a64a2e3448
amletodellaloggia <amletodellaloggia@gmail.com>
2025-07-07 15:55:54 +0200

    [edit] english [add] game function, user variable

--- 02 - Pari e Dispari/js/script.js
+++ 02 - Pari e Dispari/js/script.js
@@ -4,7 +4,13 @@
 // Ho bisogno di creare tre funzioni, una per generare un numero random per il pc, un'altra per giocare contro il pc e un'ulteriore per stabilire se il risultato della somma dei due numeri (random del pc + numero dell'utente) è pari o dispari
 
 // Creo una funzione che genera un numero random da 1 a 5 per il computer
-function generaNumeroComputer() {
+function generateComputerNumber() {
 	// Con return posso riutilizzare il numero random fuori dalla funzione
   return Math.floor(Math.random() * 5) + 1;
+}
+
+// Creo la funzione per giocare
+function playEvenOdd() {
+  // 1. Chiedo all'utente di scegliere "pari" o "dispari"
+  const sceltaUtente = prompt("Scegli pari o dispari:");
 }
